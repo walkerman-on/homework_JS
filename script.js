1015cad1f4e9d2789a35012c85c978ff4bf50436
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2022-12-21 01:25:48 +0300

    Learning arrays

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let firstArray = [1,2,4,2,4,4,5];
 let secondArray = [1,2,3,4,5];
 let thirdArray = [1,-2,-2,3,4,-5,-6,-5];
 
+// for (let i = 0; i < firstArray.length; i++) {
+//     console.log (firstArray[i])
+// }
+
+for (let key in firstArray) {
+    console.log (firstArray[key])
+}
